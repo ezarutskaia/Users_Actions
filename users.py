6992edf88003fd7fad04545529ab43b6327c1aac
Elena <ezarutskaia@gmail.com>
2023-06-26 14:15:33 +0400

    Обработка df

--- users.py
+++ users.py
@@ -16,4 +16,14 @@ file1.close
 
 df = pd.DataFrame(dictionary)
 
-print(df)
+expanded = df['location'].str.split(pat="', '", expand=True)
+df['lot'] = expanded[0]
+df['lng'] = expanded[1]
+df['city'] = expanded[2]
+
+search = lambda s: s.lstrip("['")
+df['lot'] = df['lot'].map(search)
+
+df = df.drop(columns='location')
+
+print(df.info())
