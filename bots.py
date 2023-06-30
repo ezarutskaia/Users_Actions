48dad071f88bb46ef436aacef909fee26bc82aa7
Elena <ezarutskaia@gmail.com>
2023-06-30 20:55:53 +0400

    Нахождение процентиля

--- bots.py
+++ bots.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from urllib.parse import parse_qsl
+import datetime
+import numpy as np
 
 dictionary = []
 
-file1 = open("C:/dev/python/source/bigfiles/actions.txt", "r")
+file1 = open("C:/dev/python/source/bigfiles/new/a1.txt", "r")
 
 while True:
     line = file1.readline()
@@ -16,4 +18,13 @@ file1.close
 
 df = pd.DataFrame(dictionary)
 
-print(df.head(10))
+df['time'] = pd.to_datetime(df['time'])
+
+new = df.groupby(['user']).agg({'time': ['min', 'max']}). reset_index()
+new.columns = ['user', 't_min', 't_max']
+new['t_diff'] = new['t_max'] - new['t_min']
+new['diff_sec'] = new['t_diff'].dt.total_seconds()
+
+p026 = np.percentile(new['diff_sec'], 0.26)
+
+print()
